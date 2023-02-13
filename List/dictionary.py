70023be83355d31413559332eb6d84732d077746
pnkuria <pnkuria001@gmail.com>
2023-02-13 00:03:35 -0800

    lesson 1

---
+++ List/dictionary.py
@@ -0,0 +1,31 @@
+# dictionary = {"car":["benz","bmw","ford"]}
+# print(dictionary.get("car"))
+# print(dictionary["car"])
+# car = {"model":"bmw",
+#         "year":1922
+# }
+# print(car.get("model"))
+# car.update({"year": 1945})
+# print(car.get("year"))
+# car = {"car1":{"model":"ford"},
+#        "car2":{"model":"bmw"}
+# }
+# print(car["car1"]["model"])
+# car = {"car1":{"model":"ford",
+#                "year":1936},
+#        "car2":{"model":"bmw",
+#                "year":1945}
+# }
+# print(car["car1"]["model"])
+# def limp(name):
+#      name = input("Enter your name")
+#      print("Happy Birthday "+name )  
+# limp("name")
+def sum(sum_1):
+    num1=eval(input("Enter value1"))
+    num2=eval(input("Enter value 2"))
+    sum_1= num1+num2
+    print(sum_1)
+sum("sum_1") 
+
+
--- List/pivot4.py
+++ List/pivot4.py
@@ -13,17 +13,16 @@ numbers = [12,23,34,56,12,34]
 #     print(numbers[i])
 
 # Sort
-for i in range(len(numbers)):
-    print(numbers[i])
-numbers.sort()
-print("\n")
-for i in range(len(numbers)):
-    print(numbers[i])
-
+# for i in range(len(numbers)):
+#     print(numbers[i])
+# numbers.sort()
+# print("\n")
+# for i in range(len(numbers)):
+#     print(numbers[i])
 # count
 # print(numbers.count(34))
 # Pop
-# print(numbers.pop(2))
+print(numbers.pop(2))
 # Insert
 # numbers.insert(2,98)
 # for i in range(len(numbers)):
