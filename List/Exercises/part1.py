6507ac3498008e402eb252a2d174141ffd32f835
pnkuria <pnkuria001@gmail.com>
2023-02-18 22:21:16 -0800

    assignment

--- List/Exercises/part1.py
+++ List/Exercises/part1.py
@@ -1,9 +1,21 @@
 dict = {}
-def character():
-   
-    name = input("Enter name")
-    price = input("Enter price")
-    dict["name"] = name
-   
-    print(dict)
-character()
+while True:
+    for q in range (10):
+        product = input("Enter product name")
+        if product == q:
+            break
+        else:
+            price = eval(input("Enter price of product"))
+            dict[product]= price
+    while True:
+        name = input("Enter product name:")
+        if name == "q":
+            break
+        else:
+            if name not in dict:
+                print("Not valid")
+            else:
+                print("The price of product is:",dict[name])
+    while True :
+        if price >100:
+            print(dict)
